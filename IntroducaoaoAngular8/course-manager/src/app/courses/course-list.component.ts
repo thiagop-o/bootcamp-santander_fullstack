b83e42ccb030f5d034ff2f0dc21a6492451bbb98
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-10 07:27:05 -0300

    Finalizado Modulo 'Primeiros Passos para desenvolver com angular'

--- IntroducaoaoAngular8/course-manager/src/app/courses/course-list.component.ts
+++ IntroducaoaoAngular8/course-manager/src/app/courses/course-list.component.ts
@@ -13,16 +13,16 @@ export class CourseListComponent implements OnInit{
             {
                 id: 1,
                 name: 'Angular: Forms',
-                imageUrl: '',
+                imageUrl: '/assets/images/forms.png',
                 price: 99.99,
                 code: 'XPS-8796',
                 duration: 120,
-                rating: 5.4,
+                rating: 4.5,
                 releaseDate: 'December, 2, 2019'
             },{
                 id: 2,
                 name: 'Angular: HTTP',
-                imageUrl: '',
+                imageUrl: '/assets/images/http.png',
                 price: 45.99,
                 code: 'LSL-8796',
                 duration: 80,
