23e3182adc02032119c50935168d00ea8364e61c
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-10 11:39:14 -0300

    Finalizaçao do modulo e curso

--- IntroducaoaoAngular8/course-manager/src/app/app.module.ts
+++ IntroducaoaoAngular8/course-manager/src/app/app.module.ts
@@ -2,35 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { Error404Component } from './error-404/Error-404.component';
+import { CourseModule } from './courses/course-module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
-    NavBarComponent,
-    Error404Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses' , pathMatch: 'full'
-      },
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
 
---
+++ IntroducaoaoAngular8/course-manager/src/app/shared/component/star/star.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { StarComponent } from "./star.component";
+
+@NgModule({
+    declarations: [
+        StarComponent
+    ],
+    exports: [
+        StarComponent
+    ]
+
+})
+
+export class StarModule{
+
+}
